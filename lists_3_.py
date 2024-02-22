1abbb7f6b700f51022968b629416fccdd339bf32
sergey <sl1ckypwnzip@gmail.com>
2024-02-22 14:11:52 +0300

    Добавьте файлы проекта.

---
+++ lists_3_.py
@@ -0,0 +1,8 @@
+﻿m = int(input("Максимальная масса,которую может выдержать одна лодка - "))
+n = int(input("Количество рыбаков - "))
+a =[]
+for _ in range(n):
+    a.append(int(input("Вес рыболова - ")))
+    
+print((2 * min(a) <= m) + len([x for x in a if x + min(a) > m]))
+
